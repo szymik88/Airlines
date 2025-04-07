0da2479085a325b277b6194553a7a88e9330e3f7
szymik88 <szmikulski2009@gmail.com>
2025-04-07 11:26:58 +0200

    Refactor contact form submission handling for improved clarity

--- js/contactForm.js
+++ js/contactForm.js
@@ -2,13 +2,11 @@ const form = document.querySelector('#contact form');
 
 const formSubmit = (e) => {
     e.preventDefault();
-};
-
-form.addEventListener('submit', formSubmit);
-
-form.addEventListener('submit', () => {
     const message = document.querySelector('#mail').value; // Corrected selector
     const topic = document.querySelector('#topic_of_message').value;
     const messageText = document.querySelector('#message_text').value;
+    
     console.log(message, topic, messageText);
-});
+};
+
+form.addEventListener('submit', formSubmit);
